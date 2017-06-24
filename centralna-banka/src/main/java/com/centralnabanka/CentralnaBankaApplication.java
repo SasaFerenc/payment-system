47ea9f84b3cd10104c7046387e50b89289590d78
novicasarenac <nolesarenac@yahoo.com>
2017-06-24 20:38:50 +0200

    Bootstrap project

---
+++ centralna-banka/src/main/java/com/centralnabanka/CentralnaBankaApplication.java
@@ -0,0 +1,12 @@
+package com.centralnabanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralnaBankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralnaBankaApplication.class, args);
+	}
+}
