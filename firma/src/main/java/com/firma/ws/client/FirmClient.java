ab5641c80e19f8702978f7d6f1ad97b6abf91848
Predrag <djaps94@gmail.com>
2017-06-26 02:26:00 +0200

    Introduce firm client

---
+++ firma/src/main/java/com/firma/ws/client/FirmClient.java
@@ -0,0 +1,27 @@
+package com.firma.ws.client;
+
+import com.firma.models.Nalog;
+import com.firma.models.Presek;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+/**
+ * Created by Predrag on 6/26/17.
+ */
+
+@Component
+public class FirmClient {
+
+    @Autowired
+    private WebServiceTemplate webServiceTemplate;
+
+    public void sendPayment(Nalog nalog){
+
+    }
+
+    public Presek getBankStatement(){
+
+        return null;
+    }
+}
