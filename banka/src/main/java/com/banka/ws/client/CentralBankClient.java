88ca03ae3493f3a3e444a30984b3549c3e7c871c
novicasarenac <nolesarenac@yahoo.com>
2017-06-26 22:47:08 +0200

    wsdl changed

---
+++ banka/src/main/java/com/banka/ws/client/CentralBankClient.java
@@ -0,0 +1,14 @@
+package com.banka.ws.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+@Component
+public class CentralBankClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CentralBankClient.class);
+
+}
