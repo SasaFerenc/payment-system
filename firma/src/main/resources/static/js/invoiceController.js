931fda64dbd10e01757b6cbc9789d7f085d9f06b
SasaFerenc <sasa94sm2@gmail.com>
2017-06-27 04:29:27 +0200

    Creating invoice and payment

--- firma/src/main/resources/static/js/invoiceController.js
+++ firma/src/main/resources/static/js/invoiceController.js
@@ -2,9 +2,77 @@ var app = angular.module('FirmApplication.invoiceController', []);
 
 app.controller("invoice",['$scope', function ($scope){
 
+    $scope.invoiceFormShow = false;
+    $scope.invoiceCreateButton = "Kreiraj Fakturu";
 
-    $scope.showInvoiceForm = function () {
-        alert("sdasd");
+    $scope.item = {
+        redniBroj: "",
+        nazivRobeUsluge: "",
+        kolicina: "",
+        jedinicaMere: "",
+        jedinicnaCena: "",
+        vrednost: "",
+        procenatRabata: "",
+        iznosRabata: "",
+        umanjenoZaRabat: "",
+        ukupanPorez: ""
+    }
+
+
+    $scope.invoice ={
+        idPoruke: "",
+        nazivDobavljaca: "",
+        adresaDobavljaca: "",
+        pibDobavljaca: "",
+        nazivKupca: "",
+        adresaKupca: "",
+        pibKupca: "",
+        brojRacuna: "",
+        datumRacuna: "",
+        vrednostRobe: "",
+        vrednostUsluga: "",
+        ukupnoRobaUsluga: "",
+        ukupanRabat: "",
+        ukupanPorez: "",
+        oznakaValute: "",
+        iznosZaUplatu: "",
+        uplataNaRacuna: "",
+        datumValute: "",
+        items: []
+    }
+
+
+    $scope.addItem = function () {
+
+        if($scope.item.redniBroj === undefined){
+            return;
+        }
+
+        for(var i in $scope.invoice.items){
+            if($scope.invoice.items[i].redniBroj === $scope.item.redniBroj){
+                return;
+            }
+        }
+
+        $scope.invoice.items.push(angular.copy($scope.item));
+
+    }
+
+    $scope.saveInvoice = function () {
+
+    }
+
+    $scope.deleteItem = function (item) {
+        $scope.invoice.items.splice($scope.invoice.items.indexOf(item), 1);
+    }
+    
+    $scope.showInvoiceForm = function (flag) {
+        $scope.invoiceFormShow = flag;
+        if(flag){
+            $scope.invoiceCreateButton = "Odustani";
+        }else{
+            $scope.invoiceCreateButton = "Kreiraj Fakturu";
+        }
     }
 
 
--- firma/src/main/resources/static/js/warrantController.js
+++ firma/src/main/resources/static/js/warrantController.js
@@ -2,4 +2,45 @@ var app = angular.module('FirmApplication.warrantController', []);
 
 app.controller('warrant', ['$scope', function ($scope){
 
+    $scope.warrantFormShow = false;
+    $scope.warrantCreateButton = "Kreiraj Nalog";
+
+    var podaciOPlacanju = {
+        duznikNalogodavac: "",
+        svrhaPlacanja: "",
+        primalacPoverilac: "",
+        datumNaloga: "",
+        datumValute: "",
+        racunDuznika: "",
+        modelZaduzenja: "",
+        pozivNaBrojZaduzenja: "",
+        racunPoverioca: "",
+        modelOdobrenja: "",
+        pozivNaBrojOdobrenja: "",
+        iznos: ""
+    }
+
+    $scope.warrant = {
+        idPoruke: "",
+        oznakaValute: "",
+        hitno: false,
+        podaciOPlacanju: podaciOPlacanju
+    }
+
+
+    $scope.saveWarrant = function () {
+
+    }
+
+
+
+    $scope.showWarrantForm = function (flag) {
+        $scope.warrantFormShow = flag;
+        if(flag){
+            $scope.warrantCreateButton = "Odustani";
+        }else{
+            $scope.warrantCreateButton = "Kreiraj Fakturu";
+        }
+    }
+
 }]);
