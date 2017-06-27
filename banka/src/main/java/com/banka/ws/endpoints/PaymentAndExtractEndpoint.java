6f9c198b35bbf2d96d7632ff24d31edad822fc79
novicasarenac <nolesarenac@yahoo.com>
2017-06-27 19:57:28 +0200

    Payment endpoint added

---
+++ banka/src/main/java/com/banka/ws/endpoints/PaymentAndExtractEndpoint.java
@@ -0,0 +1,36 @@
+package com.banka.ws.endpoints;
+
+import com.banka.types.Nalog;
+import com.banka.types.StringResponse;
+import com.banka.types.ZahtevZaIzvod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+
+@Endpoint
+public class PaymentAndExtractEndpoint {
+
+    private static final String NAMESPACE_URI = "http://www.paymentsystem.rs/schemas/nalog_za_placanje";
+    private static final Logger LOGGER = LoggerFactory.getLogger(PaymentAndExtractEndpoint.class);
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "nalog")
+    public StringResponse handlePaymentRequest(@RequestPayload Nalog nalog) {
+        LOGGER.info("Nalog za uplatu: " + nalog.getIdPoruke());
+        StringResponse response = new StringResponse();
+        response.setMessage("OK");
+
+        return response;
+    }
+
+    /*@PayloadRoot(namespace = NAMESPACE_URI, localPart = "izvod")
+    public StringResponse handleExtractRequest(@RequestPayload ZahtevZaIzvod zahtevZaIzvod) {
+        LOGGER.info("zahtjev za izvod");
+
+        StringResponse response = new StringResponse();
+        response.setMessage("OK");
+        return response;
+    }*/
+
+}
--- banka/src/main/java/com/banka/ws/endpoints/RtgsResponseEndpoint.java
+++ banka/src/main/java/com/banka/ws/endpoints/RtgsResponseEndpoint.java
@@ -2,7 +2,6 @@ package com.banka.ws.endpoints;
 
 import com.banka.types.Mt103;
 import com.banka.types.StringResponse;
-import com.banka.types.StringResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -10,17 +9,15 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-import javax.xml.bind.JAXBElement;
-
 @Endpoint
-public class RtsgResponseEndpoint {
+public class RtgsResponseEndpoint {
 
     private static final String NAMESPACE_URI = "http://www.paymentsystem.rs/schemas/mt103";
-    private static final Logger LOGGER = LoggerFactory.getLogger(RtsgResponseEndpoint.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RtgsResponseEndpoint.class);
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "mt103")
     @ResponsePayload
-    public StringResponse handleRtsg(@RequestPayload Mt103 mt103) {
+    public StringResponse handleRtgs(@RequestPayload Mt103 mt103) {
         LOGGER.info(mt103.getIdPoruke());
         StringResponse response = new StringResponse();
         response.setMessage("OK");
