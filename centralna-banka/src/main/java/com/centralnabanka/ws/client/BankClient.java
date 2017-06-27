1f677d56ee51efad9a5546ebdc0db4fe286ec6c5
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-06-27 09:15:35 +0200

    Log outgoing messages from bank client

--- centralna-banka/src/main/java/com/centralnabanka/ws/client/BankClient.java
+++ centralna-banka/src/main/java/com/centralnabanka/ws/client/BankClient.java
@@ -1,11 +1,15 @@
 package com.centralnabanka.ws.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 @Component
 public class BankClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BankClient.class);
 
     private WebServiceTemplate webServiceTemplate;
 
@@ -15,6 +19,8 @@ public class BankClient {
     }
 
     public void sendMessage(Object message, String bankUrl) {
+        LOGGER.info("Sending " + message.getClass().getSimpleName() + " message to " + bankUrl + " ...");
+
         webServiceTemplate.marshalSendAndReceive(bankUrl, message);
     }
 
