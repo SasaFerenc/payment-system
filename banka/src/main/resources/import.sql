9e8e2bab5e369c67c1dde9e719293f50e5367504
Nemanja Milićević <nemanja_milicevic@hotmail.com>
2017-06-29 01:26:26 +0200

    Create alternative import.sql

--- banka/src/main/resources/import.sql
+++ banka/src/main/resources/import.sql
@@ -1,8 +1,8 @@
-INSERT INTO firm(firm_name) VALUES ('FirmaA');
-INSERT INTO firm(firm_name) VALUES ('FirmaB');
+INSERT INTO firm(firm_name) VALUES ('FirmaC');
+INSERT INTO firm(firm_name) VALUES ('FirmaD');
 
-INSERT INTO account(count_number, reserved, total, firm_id) VALUES ('200111', 0, 500000, (SELECT firm_id FROM firm WHERE firm_name='FirmaA'));
---INSERT INTO account(count_number, reserved, total, firm_id) VALUES ('908111', 0, 100, (SELECT firm_id FROM firm WHERE firm_name='FirmaB'));
+INSERT INTO account(count_number, reserved, total, firm_id) VALUES ('908000', 0, 500000, (SELECT firm_id FROM firm WHERE firm_name = 'FirmaC'));
+INSERT INTO account(count_number, reserved, total, firm_id) VALUES ('908111', 0, 40000, (SELECT firm_id FROM firm WHERE firm_name = 'FirmaD'));
 
 INSERT INTO bank(name, account_number, swift_code) VALUES ('BankA', '200000000012345600', 'HAABRSBG');
 INSERT INTO bank(name, account_number, swift_code) VALUES ('BankB', '908000000001550135', 'AIKBRS22');
