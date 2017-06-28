136d45cc0a8c2a5351b31c153990f064e7fb0d33
novicasarenac <nolesarenac@yahoo.com>
2017-06-28 10:55:56 +0200

    Added endpoints for mt900 and mt210

--- banka/src/main/java/com/banka/ws/endpoints/ResponseEndpoint.java
+++ banka/src/main/java/com/banka/ws/endpoints/ResponseEndpoint.java
@@ -2,6 +2,7 @@ package com.banka.ws.endpoints;
 
 import com.banka.model.Account;
 import com.banka.services.AccountService;
+import com.banka.types.Mt102;
 import com.banka.types.Mt103;
 import com.banka.types.Mt910;
 import com.banka.types.StringResponse;
@@ -14,11 +15,13 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
-public class RtgsResponseEndpoint {
+public class ResponseEndpoint {
 
     private static final String NAMESPACE_URI_103 = "http://www.paymentsystem.rs/schemas/mt103";
+    private static final String NAMESPACE_URI_102 = "http://www.paymentsystem.rs/schemas/mt102";
     private static final String NAMESPACE_URI_910 = "http://www.paymentsystem.rs/schemas/mt910";
-    private static final Logger LOGGER = LoggerFactory.getLogger(RtgsResponseEndpoint.class);
+    private static final String NAMESPACE_URI_900 = "http://www.paymentsystem.rs/schemas/mt900";
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResponseEndpoint.class);
 
     @Autowired
     AccountService accountService;
@@ -33,6 +36,26 @@ public class RtgsResponseEndpoint {
         Account account = accountService.findByCountNumber(mt103.getPodaciOPlacanju().getRacunPoverioca()).get(0);
         account.setTotal(account.getTotal().add(mt103.getPodaciOPlacanju().getIznos()));
         accountService.save(account);
+
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI_102, localPart = "mt102")
+    @ResponsePayload
+    public StringResponse handle102(@RequestPayload Mt102 mt102) {
+        LOGGER.info("102 stigla: " + mt102.getIdPoruke());
+        StringResponse response = new StringResponse();
+        response.setMessage("OK");
+
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI_900, localPart = "mt900")
+    @ResponsePayload
+    public StringResponse handle900(@RequestPayload Mt910 mt910) {
+        LOGGER.info("900 stigla: " + mt910.getIdPoruke());
+        StringResponse response = new StringResponse();
+        response.setMessage("OK");
 
         return response;
     }
