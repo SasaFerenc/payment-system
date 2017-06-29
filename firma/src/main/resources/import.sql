0bd4c39b8e5afe123fa725917c9dd1d7631c2800
Predrag <djaps94@gmail.com>
2017-06-29 03:21:43 +0200

    Create bank model

--- firma/src/main/resources/import.sql
+++ firma/src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO firma (pib, address) VALUES ('1', 'http://127.0.0.1:8090');
 INSERT INTO firma (pib, address) VALUES ('2', 'http://192.168.1.5:9000');
+
+INSERT INTO banka (address) VALUES ('http://127.0.0.1:9010');
+INSERT INTO banka (address) VALUES ('http://192.168.1.3:8080/ws');
